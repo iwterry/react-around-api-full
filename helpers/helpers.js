234428b24cdda32886696f17fb7c833aa01da291
Isaac Terry <zeroice2007@gmail.com>
2021-03-28 13:45:32 -0500

    fix: Make regular expressions that validate link case-insensitive and add comments to explain what the validator is doing

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -49,10 +49,10 @@ function convertErrorToHttpError(err, next) {
 function doesLinkHaveValidFormat(possibleLink) {
   // from protocol to top-level domain
   // eslint-disable-next-line no-useless-escape
-  const firstPartRegEx = /^https?:\/{2}(www\.)?[a-z0-9][a-z0-9\-]*[a-z0-9]\.[a-z][a-z0-9\-]*[a-z0-9]/;
+  const firstPartRegEx = /^https?:\/{2}(www\.)?[a-z0-9][a-z0-9\-]*[a-z0-9]\.[a-z][a-z0-9\-]*[a-z0-9]/i;
   // everything after and including the / (forward slash) that follows the top-level domain
   // eslint-disable-next-line no-useless-escape
-  const lastPartRegEx = /^\/[\w\.\~\:\/\\\?\%\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=\-]*#?$/;
+  const lastPartRegEx = /^\/[\w\.\~\:\/\\\?\%\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=\-]*#?$/i;
 
   const matchForFirstPart = possibleLink.match(firstPartRegEx);
 
@@ -62,6 +62,19 @@ function doesLinkHaveValidFormat(possibleLink) {
 
   const lastPartOfPossibleLink = possibleLink.slice(matchForFirstPart[0].length);
   return lastPartOfPossibleLink === '' || lastPartRegEx.test(lastPartOfPossibleLink);
+
+  /*
+  Note: This implementation is only a loose intepretation of what a valid link format really is.
+  Due to the complexity involved, I simplified it.
+    - The valid protocals are https:// and http://
+    - The www. part is optional after https:// or http://
+    - The domain name cannot start or end with a hypen (-)
+    - The top-level domain can only start with A-Z and a-z
+    - The domain name and top-level domain must consist of only A-Z,
+      a-z, 0-9, and a hyphen
+    - The path following the top-level domain and the forward slash must
+      consists of A-Z, a-z, 0-9, and ._~:/?%#[]@!$&'()*+,;= characters
+  */
 }
 
 module.exports = {
