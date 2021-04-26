783848df6d8c9b7c1b98793b97a4c4f3bd3ef6d6
Isaac Terry <zeroice2007@gmail.com>
2021-04-25 20:47:25 -0500

    fix: change name of a property

--- errors/GeneralServerHttpError.js
+++ errors/GeneralServerHttpError.js
@@ -3,7 +3,7 @@ const HttpError = require('./HttpError');
 class GeneralServerHttpError extends HttpError {
   constructor(origError, message = 'An error has occurred on our end') {
     super('GeneralServerHttpError', 500, message);
-    this.otherError = origError;
+    this.origError = origError;
   }
 }
 
