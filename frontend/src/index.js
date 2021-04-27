ec7a2de265cbfcb03824765bdeede0fe2cc0aab0
Isaac Terry <zeroice2007@gmail.com>
2021-04-27 04:51:59 -0500

    remove use of path /iwterry/react-around-api-full for use of frontend code when serving

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/iwterry/react-around-api-full">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
