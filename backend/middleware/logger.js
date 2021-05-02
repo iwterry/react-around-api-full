53e26a95eb5bd253e671b9daa5e9509eed968c5b
Isaac Terry <zeroice2007@gmail.com>
2021-05-02 04:33:37 -0500

    have log files in 'logs' subdirectory

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -3,14 +3,14 @@ const expressWinston = require('express-winston');
 
 module.exports.requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: 'logs/request.log' }),
   ],
   format: winston.format.json(),
 });
 
 module.exports.errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: 'logs/error.log' }),
   ],
   format: winston.format.json(),
 });
